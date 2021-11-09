0e9dc33daa0c04784c264a272239dc654eab9465
bruno <bruno@DESKTOP-1HQHQMB>
2021-11-09 13:09:14 -0300

    Adicionar arquivos de projeto.

---
+++ Ativ5/Models/Imc1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ativ5.Models
+{
+    public class Imc1
+    {
+        public double IMC { get; set; }
+        public double ImcMaior { get; set; }
+        public double ImcMenor { get; set; }
+        public List<double> participantes { get; set; }
+    }
+}
---
+++ Ativ5/Program.cs
@@ -0,0 +1,68 @@
+﻿using Ativ5.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Ativ5
+{
+    class Program
+    {
+        public static IEnumerable<object> ImcFinal { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo à calculadora de IMC");
+            List<double> participantes = new List<double>();
+            var count = 6;
+            for (var i = 1; i < count; i++)
+            {
+                Console.WriteLine("Digita o nome, a idade, o peso(em kg) e a altura(em metros) da pessoa Nº" + i);
+                string participante = Console.ReadLine();
+                int idade = Int32.Parse(Console.ReadLine());
+                double peso = double.Parse(Console.ReadLine());
+                double altura = double.Parse(Console.ReadLine());
+                var ImcFinal = CalculateImc(peso, altura, participantes);                               
+                Console.WriteLine("O IMC do " + participante + " é " + ImcFinal.IMC);              
+                double participanteIMC = ImcFinal.IMC;                
+                participantes.Add(participanteIMC);
+                var x = i + 1;
+                
+                if(x==6)
+                {
+                    Console.Clear();
+                    Console.WriteLine("O menor imc é de "+ ImcFinal.ImcMenor);
+                    Console.WriteLine("O maior imc é " + ImcFinal.ImcMaior);
+                }
+            }
+
+        }
+
+        public static Imc1 CalculateImc(double peso, double altura, List<double>participantes)
+        {   
+            
+            var imcResult = new Imc1();
+            imcResult.ImcMaior = 0;
+            imcResult.ImcMenor = 0;
+            imcResult.IMC = peso / Math.Pow(altura, 2);     
+            
+            foreach (double participanteIMC in participantes)
+            {
+                if (participanteIMC >= imcResult.ImcMaior)
+                {
+                    imcResult.ImcMaior = participanteIMC;
+
+                }
+                if (imcResult.ImcMenor == 0)
+                {
+                    imcResult.ImcMenor = participanteIMC;
+                }
+                if (participanteIMC <= imcResult.ImcMenor)
+                {
+                    imcResult.ImcMenor = participanteIMC;
+                }        
+
+            }
+
+            return imcResult;
+        }
+    } 
+}
